9f14ca5dbb2c0e27463a96b2dea17c086f72f0aa
carlos menacho <menachocarlos5@gmail.com>
2024-08-12 07:36:40 -0400

    Adding integration with to SAM2 and YOLO

--- annotation/annotate.py
+++ annotation/annotate.py
@@ -1,11 +1,32 @@
+from segment_extraction import DetectAndSegment
+from sklearn.model_selection import train_test_split
+import yaml
 import os
+import cv2
 import numpy as np
-import torch
 import matplotlib.pyplot as plt
 from PIL import Image
-from sam2.build_sam import build_sam2
-import yaml
 
+class GenerateDataset:
+    def __init__(self, 
+                recording_path, 
+                output_path, 
+                test_size = 0.2, 
+                val_size=0.1, 
+                random_state = 42):
+        self.recording_path = recording_path
+        self.output_path = output_path
+        self.test_size = test_size
+        self.val_size = val_size
+        self.random_state = random_state
+
+    def process_samples(self):
+
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path)
+
+        file_list = os.listdir(self.recording_path)
+        
 
 
 def main():
@@ -15,7 +36,36 @@ def main():
     if os.path.exists(config['sam2']['model'] and config['sam2']['config']): 
         sam2_checkpoint = config['sam2']['model']
         model_cfg = config['sam2']['config']
-        predictor = build_sam2(model_cfg, sam2_checkpoint)
+        yolo_model = config['yolo']['modelpath']
+
+        print(sam2_checkpoint)
+        print(model_cfg)
+        print(yolo_model)
+
+        #init
+
+        segmentor = DetectAndSegment(
+            yolo_model_path=yolo_model, 
+            sam2_checkpoint=sam2_checkpoint, 
+            sam2_model=model_cfg
+            )
+        
+        if not os.path.exists(yolo_model):
+            segmentor.download_yolo_model()
+
+        # load an image
+        image = cv2.imread('me.jpeg')
+        image =  cv2.resize(image, (480,720))
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        mask = segmentor.sam2_img_inference(rgb_image)
+        
+        
+        cv2.imshow("frame", image)
+        cv2.imshow("mask", mask[0]*255)
+        cv2.waitKey()
+
+
     else: 
         print('files does not exist')
 
--- annotation/segment_extraction.py
+++ annotation/segment_extraction.py
@@ -3,11 +3,16 @@ from sam2.sam2_image_predictor import SAM2ImagePredictor
 from sahi.utils.yolov8 import download_yolov8s_model
 from ultralytics import YOLO
 import torch
+import numpy as np
 import os 
 import ffmpeg
 
 class DetectAndSegment:
     def __init__(self, yolo_model_path, sam2_checkpoint, sam2_model):
+
+        self.yolo_model_path = yolo_model_path
+        self.sam2_checkpoint = sam2_checkpoint
+        self.sam2_model = sam2_model
 
         torch.autocast(device_type='cuda', dtype=torch.bfloat16).__enter__()
         if torch.cuda.get_device_properties(0).major >= 8:
@@ -27,12 +32,16 @@ class DetectAndSegment:
             #video predictor
             self.vid_predictor = build_sam2_video_predictor(self.sam2_model, self.sam2_checkpoint)
             #image predictor        
-            self.img_predictor = SAM2ImagePredictor(build_sam2(self))
-        
+            self.img_predictor = SAM2ImagePredictor(build_sam2(self.sam2_model, self.sam2_checkpoint))
+        else: 
+            print("Directory does not exist")
 
-    def download_model(self):
+    def download_yolo_model(self):
+        
+        if not os.path.exists('models'):
+            os.makedirs('models')
+
         download_yolov8s_model(self.yolo_model_path)
-        print(f"YOLOv8 model downloaded to {self.yolo_model_path}")
 
     def yolo_inference(self, img_path):
         results = self.model(img_path, stream = False)
@@ -49,13 +58,15 @@ class DetectAndSegment:
         inference_state = self.vid_predictor.init_state(video_path = video_path)
         self.vid_predictor.reset_state(inference_state)
 
-    def sam2_img_inference(self, img, bboxs):
+    def sam2_img_inference(self, img):
+
+        bboxes = np.array(self.yolo_inference(img))   
         with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
             self.img_predictor.set_image(img)
             masks, _, _ = self.img_predictor.predict(
                 point_coords=None,
                 point_labels=None,
-                box=bboxs[None, :],
+                box=bboxes[None, :],
                 multimask_output=False,
             )
 
